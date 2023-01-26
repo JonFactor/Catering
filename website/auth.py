d25c3462730be3e21d2ed2040458bd800cb59d80
JonFactor <Jon.Factor2@gmail.com>
2023-01-26 13:04:19 -0500

    idkman

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,17 @@
 # imports
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # define this file is a blueprint
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return '1'
+    return render_template('login.html',boolean = True)
 
-@auth.route('/Logout')
-def Logout():
-    return '2'
+@auth.route('/logout')
+def logout():
+    return '<t>Logging OUT</t>'
 
 @auth.route('/SignUp')
-def SignUp():
-    return '3'
+def signup():
+    return render_template('signup.html')
